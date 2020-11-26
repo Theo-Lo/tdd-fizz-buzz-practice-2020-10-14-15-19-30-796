d98bfb549c5bf0df5dfa5d615468a5768f733c81
Theo Lo <theo.lo@oocl.com>
2020-11-26 14:07:42 +0800

    [FizzBuzz][Theo]refactor: Change string value into constant variable

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,10 +1,11 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public static final String FIZZ = "Fizz";
     public String countOff(int number) {
         String string = "";
         if(number%3 == 0){
-            return string.concat("Fizz");
+            return string.concat(FIZZ);
         }
         return String.valueOf(number);
     }
