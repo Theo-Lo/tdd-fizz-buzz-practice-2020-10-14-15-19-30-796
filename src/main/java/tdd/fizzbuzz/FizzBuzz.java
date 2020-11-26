67225228ec31eb5b0acd389d5a0a4b09f687712b
Theo Lo <theo.lo@oocl.com>
2020-11-26 14:11:46 +0800

    [FizzBuzz][Theo]feature: implement FizzBuzz to return Buzz when order is multiple of 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,10 +2,14 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
     public String countOff(int number) {
         String string = "";
         if(number%3 == 0){
             return string.concat(FIZZ);
+        }
+        if(number%5 == 0){
+            return string.concat(BUZZ);
         }
         return String.valueOf(number);
     }
