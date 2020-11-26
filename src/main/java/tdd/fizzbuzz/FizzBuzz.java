38f7d240a2302cfac56638276f2c305270767744
Theo Lo <theo.lo@oocl.com>
2020-11-26 14:03:46 +0800

    [FizzBuzz][Theo]feature: implement FizzBuzz to return Fizz when order is multiple of 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,10 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int number) {
+        String string = "";
+        if(number%3 == 0){
+            return string.concat("Fizz");
+        }
         return String.valueOf(number);
     }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -25,4 +25,6 @@ public class FizzBuzzTest {
         //Then
         assertEquals("Fizz", order);
     }
+
+
 }
