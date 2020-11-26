a22f94eb82db90a731418f0807bbbb10c414c68d
Theo Lo <theo.lo@oocl.com>
2020-11-26 14:14:10 +0800

    [FizzBuzz][Theo]feature: implement FizzBuzz to output Whizz when order is multiple of 7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,7 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
     public String countOff(int number) {
         String string = "";
         if(number%3 == 0){
@@ -10,6 +11,9 @@ public class FizzBuzz {
         }
         if(number%5 == 0){
             return string.concat(BUZZ);
+        }
+        if(number%7 == 0){
+            return string.concat(WHIZZ);
         }
         return String.valueOf(number);
     }
