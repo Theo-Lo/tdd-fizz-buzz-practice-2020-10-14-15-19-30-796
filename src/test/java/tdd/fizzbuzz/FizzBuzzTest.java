d274eb5a62c22fabda6ef35387b9b3fec48c365a
Theo Lo <theo.lo@oocl.com>
2020-11-26 13:54:47 +0800

    [FizzBuzz][Theo]test: create unit test for output normal order

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,17 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_normal_number_when_order_is_given(){
+        //Given
+        int number = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //When
+        String order = fizzBuzz.countOff(number);
+        //Then
+        assertEquals("1", order);
+    }
 }
